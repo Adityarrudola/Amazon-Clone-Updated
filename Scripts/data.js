05089cad8fc4ad8505ba55283451a1daa4424123
Aditya Rudola <99611034+Adityarrudola@users.noreply.github.com>
2025-06-07 14:06:59 +0530

    Add files via upload

---
+++ Scripts/data.js
@@ -0,0 +1,364 @@
+const Products1 = [
+  {
+    id: "amazonproject-item1",
+    image: "Clothes/img1.webp",
+    heading: "Men's Classic White T-shirt",
+    subheading: "Essential White T-shirt for Men, Comfortable and Stylish",
+    price: 299,
+  },
+  {
+    id: "amazonproject-item2",
+    image: "Clothes/img2.webp",
+    heading: "Men's Graphic Print T-shirt",
+    subheading: "Trendy Graphic Print T-shirt for Men, Various Designs",
+    price: 349,
+  },
+  {
+    id: "amazonproject-item3",
+    image: "Clothes/img3.webp",
+    heading: "Men's Polo T-shirt",
+    subheading: "Classic Polo T-shirt for Men, Casual and Versatile",
+    price: 399,
+  },
+  {
+    id: "amazonproject-item4",
+    image: "Clothes/img4.webp",
+    heading: "Men's Striped T-shirt",
+    subheading: "Striped T-shirt for Men, Stylish and Smart Casual",
+    price: 349,
+  },
+  {
+    id: "amazonproject-item5",
+    image: "Clothes/img5.webp",
+    heading: "Men's V-neck T-shirt",
+    subheading: "V-neck T-shirt for Men, Perfect for Layering",
+    price: 299,
+  },
+  {
+    id: "amazonproject-item6",
+    image: "Clothes/img6.webp",
+    heading: "Men's Crew Neck T-shirt",
+    subheading: "Crew Neck T-shirt for Men, Ideal for Everyday Wear",
+    price: 299,
+  },
+];
+
+const Products2 = [
+  {
+    id: "amazonproject-item7",
+    image: "health/img1.webp",
+    heading: "Health Boost Supplements",
+    subheading:
+      "Nutritional Supplements for a Healthy Lifestyle, Improve Your Well-being",
+    price: 799,
+  },
+  {
+    id: "amazonproject-item8",
+    image: "health/img2.webp",
+    heading: "Vitamins and Minerals",
+    subheading: "Essential Nutrients for Your Body, Stay Fit and Energetic",
+    price: 499,
+  },
+  {
+    id: "amazonproject-item9",
+    image: "health/img3.webp",
+    heading: "Protein Powder",
+    subheading: "Fuel Your Muscles with High-Quality Protein Powder",
+    price: 599,
+  },
+  {
+    id: "amazonproject-item10",
+    image: "health/img4.webp",
+    heading: "Healthy Snack Bars",
+    subheading: "Delicious and Nutritious Snacks for On-the-Go",
+    price: 299,
+  },
+  {
+    id: "amazonproject-item11",
+    image: "health/img5.webp",
+    heading: "Fitness Tracker Watch",
+    subheading: "Track Your Health and Fitness Goals with This Smartwatch",
+    price: 999,
+  },
+  {
+    id: "amazonproject-item12",
+    image: "health/img6.webp",
+    heading: "Yoga Mat",
+    subheading: "High-Quality Yoga Mat for Your Daily Practice",
+    price: 199,
+  },
+];
+
+const Products3 = [
+  {
+    id: "amazonproject-item13",
+    image: "furniture/img1.jpg",
+    heading: "Elegant Living Room Sofa",
+    subheading: "High-Quality Comfortable Sofa for Your Home, Relax in Style",
+    price: 1999,
+  },
+  {
+    id: "amazonproject-item14",
+    image: "furniture/img2.jpg",
+    heading: "Modern Dining Table Set",
+    subheading: "Stylish Dining Furniture, Gather Your Family and Friends",
+    price: 1499,
+  },
+  {
+    id: "amazonproject-item15",
+    image: "furniture/img3.jpg",
+    heading: "Cozy Bedroom Furniture",
+    subheading: "Create a Relaxing Bedroom Space with This Furniture Set",
+    price: 799,
+  },
+  {
+    id: "amazonproject-item16",
+    image: "furniture/img4.jpg",
+    heading: "Office Desk and Chair",
+    subheading: "Productive Workstation Furniture for Your Home Office",
+    price: 699,
+  },
+  {
+    id: "amazonproject-item17",
+    image: "furniture/img5.jpg",
+    heading: "Outdoor Patio Set",
+    subheading: "Enjoy Outdoor Living with This Patio Furniture Collection",
+    price: 1299,
+  },
+  {
+    id: "amazonproject-item18",
+    image: "furniture/img6.jpg",
+    heading: "Kids' Bunk Bed",
+    subheading: "Space-Saving and Fun Bunk Bed for Kids' Bedrooms",
+    price: 599,
+  },
+];
+
+const Products4 = [
+  {
+    id: "amazonproject-item19",
+    image: "smartphone/img1.jpg",
+    heading: "Flagship Smartphone X",
+    subheading: "High-Performance Smartphone with Advanced Features",
+    price: 899,
+  },
+  {
+    id: "amazonproject-item20",
+    image: "smartphone/img2.webp",
+    heading: "Budget-Friendly Smartphone Y",
+    subheading: "Affordable Smartphone with Impressive Specifications",
+    price: 399,
+  },
+  {
+    id: "amazonproject-item21",
+    image: "smartphone/img3.jpg",
+    heading: "Camera Phone Z",
+    subheading: "Capture Stunning Photos and Videos with This Camera Phone",
+    price: 749,
+  },
+  {
+    id: "amazonproject-item22",
+    image: "smartphone/img4.jpg",
+    heading: "Compact Mobile Phone",
+    subheading: "Small and Lightweight Phone for Portability",
+    price: 299,
+  },
+  {
+    id: "amazonproject-item23",
+    image: "smartphone/img5.jpg",
+    heading: "Business-Ready Smartphone",
+    subheading: "Stay Productive with This Business-Oriented Smartphone",
+    price: 599,
+  },
+  {
+    id: "amazonproject-item24",
+    image: "smartphone/img6.jpg",
+    heading: "Gaming Phone",
+    subheading: "Enhance Your Gaming Experience with This Gaming Smartphone",
+    price: 699,
+  },
+];
+
+const Products5 = [
+  {
+    id: "amazonproject-item25",
+    image: "cosmetics/img1.jpg",
+    heading: "Beauty Essentials Set",
+    subheading: "Complete Set of Cosmetics for Your Everyday Look",
+    price: 299,
+  },
+  {
+    id: "amazonproject-item26",
+    image: "cosmetics/img2.jpg",
+    heading: "Skincare Routine Kit",
+    subheading: "Skin-Friendly Products to Enhance Your Natural Beauty",
+    price: 199,
+  },
+  {
+    id: "amazonproject-item27",
+    image: "cosmetics/img3.jpg",
+    heading: "Lipstick Collection",
+    subheading: "Variety of Shades to Express Your Unique Style",
+    price: 49,
+  },
+  {
+    id: "amazonproject-item28",
+    image: "cosmetics/img4.jpg",
+    heading: "Perfume Gift Set",
+    subheading: "Elegant Fragrances for a Lasting Impression",
+    price: 79,
+  },
+  {
+    id: "amazonproject-item29",
+    image: "cosmetics/img5.jpg",
+    heading: "Makeup Brushes Bundle",
+    subheading: "Professional Brushes for Precise Makeup Application",
+    price: 89,
+  },
+  {
+    id: "amazonproject-item30",
+    image: "cosmetics/img6.jpg",
+    heading: "Nail Polish Collection",
+    subheading: "Vibrant Nail Colors for Perfect Manicures",
+    price: 29,
+  },
+];
+
+const Products6 = [
+  {
+    id: "amazonproject-item31",
+    image: "petcare/img1.jpg",
+    heading: "Pet Grooming Kit",
+    subheading:
+      "Keep Your Furry Friend Looking Great with These Grooming Tools",
+    price: 49,
+  },
+  {
+    id: "amazonproject-item32",
+    image: "petcare/img2.jpg",
+    heading: "Premium Dog Food",
+    subheading: "High-Quality Nutritious Food for Your Beloved Dog",
+    price: 29,
+  },
+  {
+    id: "amazonproject-item33",
+    image: "petcare/img3.jpg",
+    heading: "Cat Litter Box",
+    subheading: "Convenient Litter Box for Your Feline Companion",
+    price: 39,
+  },
+  {
+    id: "amazonproject-item34",
+    image: "petcare/img4.jpg",
+    heading: "Pet Toys Assortment",
+    subheading: "Interactive Toys to Keep Your Pets Entertained",
+    price: 19,
+  },
+  {
+    id: "amazonproject-item35",
+    image: "petcare/img5.jpg",
+    heading: "Fish Tank Starter Kit",
+    subheading: "Set Up Your Own Aquatic World with This Fish Tank Kit",
+    price: 59,
+  },
+  {
+    id: "amazonproject-item36",
+    image: "petcare/img6.jpg",
+    heading: "Bird Cage",
+    subheading: "Spacious Cage for Your Feathered Friends",
+    price: 69,
+  },
+];
+
+const Products7 = [
+  {
+    id: "amazonproject-item37",
+    image: "toys/img1.jpg",
+    heading: "Educational Toys for Kids",
+    subheading: "Fun and Learning Combined with These Educational Toys",
+    price: 19,
+  },
+  {
+    id: "amazonproject-item38",
+    image: "toys/img2.jpg",
+    heading: "Outdoor Playtime Set",
+    subheading: "Encourage Outdoor Activities with This Playtime Set",
+    price: 39,
+  },
+  {
+    id: "amazonproject-item39",
+    image: "toys/img3.jpg",
+    heading: "Building Blocks Kit",
+    subheading: "Improve Creativity with These Colorful Building Blocks",
+    price: 29,
+  },
+  {
+    id: "amazonproject-item40",
+    image: "toys/img4.jpg",
+    heading: "Dollhouse with Accessories",
+    subheading: "Create Magical Stories with This Dollhouse Set",
+    price: 49,
+  },
+  {
+    id: "amazonproject-item41",
+    image: "toys/img5.jpg",
+    heading: "Remote Control Car",
+    subheading: "Experience Thrilling Adventures with This RC Car",
+    price: 59,
+  },
+  {
+    id: "amazonproject-item42",
+    image: "toys/img6.jpg",
+    heading: "Puzzle Collection",
+    subheading: "Challenge Your Mind with These Engaging Puzzles",
+    price: 19,
+  },
+];
+
+const Products8 = [
+  {
+    id: "amazonproject-item43",
+    image: "fasion/img1.jpg",
+    heading: "Stylish Men's Shirt",
+    subheading: "Upgrade Your Wardrobe with This Trendy Men's Shirt",
+    price: 49,
+  },
+  {
+    id: "amazonproject-item44",
+    image: "fasion/img2.jpg",
+    heading: "Women's Fashion Dress",
+    subheading: "Look Elegant and Stylish in This Beautiful Dress",
+    price: 59,
+  },
+  {
+    id: "amazonproject-item45",
+    image: "fasion/img3.jpg",
+    heading: "Classic Leather Jacket",
+    subheading: "Timeless Style with This Leather Jacket for Men",
+    price: 149,
+  },
+  {
+    id: "amazonproject-item46",
+    image: "fasion/img4.jpg",
+    heading: "Designer Handbag",
+    subheading: "Accessorize Your Outfits with This Elegant Handbag",
+    price: 79,
+  },
+  {
+    id: "amazonproject-item47",
+    image: "fasion/img5.jpg",
+    heading: "Athletic Shoes",
+    subheading: "Stay Active and Comfortable with These Athletic Shoes",
+    price: 69,
+  },
+  {
+    id: "amazonproject-item48",
+    image: "fasion/img6.jpg",
+    heading: "Designer Sunglasses",
+    subheading: "Protect Your Eyes in Style with These Sunglasses",
+    price: 39,
+  },
+];
+
+
+const allProducts = [Products1, Products2, Products3, Products4, Products5, Products6, Products7, Products8];
